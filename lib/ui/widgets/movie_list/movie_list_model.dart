8401a918f35310cec3e40d093da045c79448cd3f
Санжар Ирисалиев <sanzaririsaliev@MacBook-Pro.local>
2022-07-29 02:15:59 +0600

    pagination

--- lib/ui/widgets/movie_list/movie_list_model.dart
+++ lib/ui/widgets/movie_list/movie_list_model.dart
@@ -8,6 +8,9 @@ import '../../../domain/entity/movie.dart';
 class MovieListModel extends ChangeNotifier {
   final _apiClient = ApiClient();
   final _movies = <Movie>[];
+  late int _currentPage;
+  late int _totalPage;
+  var _isLoadingInProgres = false;
   List<Movie> get movies => List.unmodifiable(_movies);
   late DateFormat _dateFormat;
   String _locale = "";
@@ -19,19 +22,37 @@ class MovieListModel extends ChangeNotifier {
     if (_locale == locale) return;
     _locale = locale;
     _dateFormat = DateFormat.yMMMMd(locale);
+    _currentPage = 0;
+    _totalPage = 1;
     _movies.clear();
     _loadMovies();
   }
 
   Future<void> _loadMovies() async {
-    final moviesResponse = await _apiClient.popularMovie(1, _locale);
-    _movies.addAll(moviesResponse.movies);
-    notifyListeners();
+    if (_isLoadingInProgres || _currentPage >= _totalPage) return;
+    _isLoadingInProgres = true;
+    final nextPage = _currentPage + 1;
+
+    try {
+      final moviesResponse = await _apiClient.popularMovie(nextPage, _locale);
+      _movies.addAll(moviesResponse.movies);
+      _currentPage = moviesResponse.page;
+      _totalPage = moviesResponse.totalPages;
+      _isLoadingInProgres = false;
+      notifyListeners();
+    } catch (e) {
+      _isLoadingInProgres = false;
+    }
   }
 
   void onMovieTap(BuildContext context, int index) {
     final id = _movies[index].id;
     Navigator.of(context)
         .pushNamed(MainNavigationRouteNames.movieDetails, arguments: id);
+  }
+
+  void showedMovieAtIndex(int index) {
+    if (index < _movies.length - 1) return;
+    _loadMovies();
   }
 }
